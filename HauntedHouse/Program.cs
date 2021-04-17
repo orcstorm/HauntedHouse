4656c2d93f40802ea1efae3088fa24594018980e
Donald Mennerich <don@orcstorm.net>
2021-04-17 17:42:43 -0400

    Add project files.

---
+++ HauntedHouse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HauntedHouse
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new HauntedHouseGame())
+                game.Run();
+        }
+    }
+}
